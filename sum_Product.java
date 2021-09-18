5aa7da3c8ed65e0dd80b61de602e419da99f2859
Sadam452 <hussainganie388@gmail.com>
2021-09-18 18:55:50 +0530

    sum & product

---
+++ sum_Product.java
@@ -0,0 +1,16 @@
+//java program to find sum & product of two numbers
+//Author @sadam
+import java.util.*;
+class sum_Product{
+	public static void main(String ar[])
+	{
+		int num1,num2,sum=0,product=0;
+		System.out.println("Enter any two numbers");
+		Scanner Sc =new Scanner(System.in);
+		num1 = Sc.nextInt();
+		num2 = Sc.nextInt();
+		sum = num1 + num2;
+		product = num1 * num2;
+		System.out.println(num1+" + "+num2+" = "+sum+"\r\n"+num1+" * "+num2+" = "+product);
+	}
+}
