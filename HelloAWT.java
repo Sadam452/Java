457cf8fce608bbda4241dfbe0de8835fed3c837c
Sadam452 <hussainganie388@gmail.com>
2021-12-07 15:49:42 +0530

    AWT demonstration
    Simple example of AWT
    #FROM BOOK

---
+++ HelloAWT.java
@@ -0,0 +1,25 @@
+import java.util.*;
+import java.awt.event.*;
+public class HelloAWT extends Frame { 
+Label contents;
+Button dispbutton;
+public HelloAWT() { 
+setLayout(new FlowLayout(FlowLayout.CENTER, 50, 50));
+contents = new Label(" "); 
+add(contents); 
+dispbutton = new Button("Show"); 
+dispbutton.addActionListener(new DispButtonListener()); 
+add(dispbutton);
+}
+class DispButtonListener implements ActionListener { 
+public void actionPerformed(ActionEvent e) {
+contents.setText("Hello World!");
+}
+}
+public static void main (String[] args) {
+HelloAWT f = new HelloAWT();
+f.setTitle("Hello!");
+f.setSize(400,150);
+f.setVisible(true);
+}
+}
