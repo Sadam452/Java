76393fc181c0abde3a6dca0d344753b0df34b645
Sadam452 <hussainganie388@gmail.com>
2021-12-07 15:54:12 +0530

    bitwise, exceptions

---
+++ bitwise.java
@@ -0,0 +1,12 @@
+/** Author @ sadam
+ * program to demonstrate various bitwise operations*/
+import java.util.*;
+class bitwise
+{
+	public static void main(String args[])
+	{
+		float c = 10.0;
+		float x = c/2;
+		system.out.println(x);
+	}
+}
